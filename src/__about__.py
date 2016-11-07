0f6bc766fee8157096adacd53272b0ab9f8438c1
Augustin Borsu <augustin@sagacify.com>
2016-11-07 10:50:08 +0100

    refactor(packaging): use __about__.py

---
+++ src/__about__.py
@@ -0,0 +1,20 @@
+# inspired from:
+# https://github.com/pypa/warehouse/blob/master/warehouse/__about__.py
+
+__all__ = [
+    "__title__", "__summary__", "__uri__", "__version__",
+    "__author__", "__email__", "__license__", "__copyright__",
+]
+
+__title__ = "sagalogger"
+__summary__ = "A json log formatter."
+__uri__ = "https://www.github.com/Sagacify/logger-py"
+
+__version__ = "0.2.1"
+
+
+__author__ = "Sagacify"
+__email__ = "dev+python@sagacify.com"
+
+__license__ = "MIT"
+__copyright__ = "2016 %s" % __author__
